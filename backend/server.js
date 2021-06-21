7d9a0849a35e3c8f25e229c9fc36d5bf4214a396
unknown <a-f-r@hotmail.es>
2021-06-21 23:00:15 +0200

    Add API endpoints routes

--- backend/server.js
+++ backend/server.js
@@ -18,19 +18,24 @@ const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
 });
 
+//6º Define routes
+app.use('/exercises', require('./routes/exercises'));
+app.use('/users', require('./routes/users'));
+
 // get driver connection
 // const dbo = require('./db/conn');
 
-// 6º Define port
+// 7º Define port
 const port = process.env.PORT || 5000;
 
-// 7º Listener
+// 8º Listener
 app.listen(port, () => {
   // perform a database connection when server starts
   //   dbo.connectToServer(function (err) {
