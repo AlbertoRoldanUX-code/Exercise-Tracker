123213f22a2a9ccdfbb2c9410f723067cd19d2a8
unknown <a-f-r@hotmail.es>
2021-06-22 19:34:57 +0200

    Add constructor and methods into creating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Navbar from "./components/Navbar"
-import ExercisesList from "./components/ExercisesList"
-import EditExercise from "./components/EditExercise"
-import CreateExercise from "./components/CreateExercise"
-import CreateUser from "./components/CreateUser"
+import Navbar from './components/Navbar';
+import ExercisesList from './components/ExercisesList';
+import EditExercise from './components/EditExercise';
+import CreateExercise from './components/CreateExercise';
+import CreateUser from './components/CreateUser';
 
 function App() {
-  return 
-
-  <Router>
-  <Navbar />
-  <br></br>
-  <Route path="/" exact component={ExercisesList}/>
-  <Route path="/edit/:id" component={EditExercise}/>
-  <Route path="/create" component={CreateExercise}/>
-  <Route path="/user" component={CreateUser}/>
-  </Router>
+  return (
+    <Router>
+      <div className='container'>
+        <Navbar />
+        <br></br>
+        <Route path='/' exact component={ExercisesList} />
+        <Route path='/edit/:id' component={EditExercise} />
+        <Route path='/create' component={CreateExercise} />
+        <Route path='/user' component={CreateUser} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
